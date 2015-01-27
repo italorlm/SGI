c635f016cca90acb8dd210d670547c8dfa5f8996
Italo Medeiros <italo.medeiros.rlm@gmail.com>
2015-01-26 23:19:01 -0300

    Adição do atributo MAPs.

--- sgi/src/controller/ProgramaController.java
+++ sgi/src/controller/ProgramaController.java
@@ -1,15 +1,20 @@
 package controller;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 
 import model.Cargo;
 import model.Cidadao;
 import model.Contrato;
+import model.ContratoParcela;
 import model.MaterialPermanente;
 import model.Programa;
+import model.ProgramaMap;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -17,17 +22,36 @@ import org.springframework.stereotype.Component;
 import dao.CargoDao;
 import dao.ContratoDao;
 import dao.ProgramaDao;
+import dao.ProgramaMapDao;
 
 @Component
 @Scope("globalSession")
 public class ProgramaController extends GenericController<Programa, ProgramaDao> {
-
-	List<SelectItem> selectItems;
+	
 	final static String DAO_CONCRETO = "programaDaoImp";
+	
+	ProgramaMap programaMap;
+	
+	boolean mostrarModalMap;
+	
+	List<ProgramaMap> programaMaps, programaMapExcluidos;
+	List<SelectItem> selectItems;
+	
+	@Resource
+	ProgramaMapDao programaMapDao;
 
 	public ProgramaController(){
 		injetaDao();		
 		filtro = new Programa();
+		trazerTodos = true;
+	}
+	
+	@Override
+	public void limpar() throws InstantiationException, IllegalAccessException {
+		selectItems = new ArrayList<SelectItem>();
+		programaMaps = new ArrayList<ProgramaMap>();
+		programaMapExcluidos = new ArrayList<ProgramaMap>();		
+		super.limpar();
 	}
 
 	@Override
@@ -49,6 +73,72 @@ public class ProgramaController extends GenericController<Programa, ProgramaDao>
 		trazerTodos = false;
 		listagem = dao.findByPrograma(filtro);
 	}
+	
+	@Override
+	public String salvar() {
+		String retorno;
+		
+		try {
+			retorno = super.salvar();
+			
+			if(!programaMaps.isEmpty()) {
+				for(ProgramaMap programaMap : programaMaps) {
+					if(objeto.getId()!=null) {
+						programaMap.setPrograma(objeto);
+						programaMapDao.salvarOuAtualizar(programaMap);
+					}				
+				}
+			}
+			
+			if(!programaMapExcluidos.isEmpty()) {
+				for(ProgramaMap programaMap : programaMapExcluidos) {
+					if(programaMap.getId()!=null) {
+						programaMapDao.excluir(programaMap);
+					}
+				}
+			}
+		} catch(Exception e) {
+			retorno = null;
+		}
+		
+		return retorno;
+	}
+	
+	@Override
+	public String editar() {
+		programaMaps = programaMapDao.findByPrograma(objeto);
+		return super.editar();
+	}
+	
+	public void mostrarModalMap(ActionEvent event) {
+		programaMap = new ProgramaMap();
+		mostrarModalMap = true;
+	}
+	
+	public void adicionarMap() {
+		programaMaps.add(programaMap);
+		mostrarModalMap = false;
+	}
+	
+	public void excluirMap() {
+		if(!programaMaps.isEmpty()) {
+			for(Iterator<ProgramaMap> i = programaMaps.iterator(); i.hasNext();) {
+				if(i.next().equals(programaMap))
+					i.remove();
+			}
+		}
+		
+		programaMapExcluidos.add(programaMap);
+	}
+	
+	@Override
+	public void filtrarSuggestionBox(String userInput) {
+		for(Programa programa : getListagem()) {
+			if(programa.getNome().toLowerCase().startsWith(userInput.toLowerCase()))
+				if(!suggestions.contains(programa))
+					suggestions.add(programa);
+		}
+	}
 
 	public List<SelectItem> getSelectItems() {
 		selectItems = new ArrayList<SelectItem>();
@@ -63,14 +153,39 @@ public class ProgramaController extends GenericController<Programa, ProgramaDao>
 	public void setSelectItems(List<SelectItem> selectItems) {
 		this.selectItems = selectItems;
 	}
-	
-	@Override
-	public void filtrarSuggestionBox(String userInput) {
-		for(Programa programa : getListagem()) {
-			if(programa.getNome().toLowerCase().startsWith(userInput.toLowerCase()))
-				if(!suggestions.contains(programa))
-					suggestions.add(programa);
-		}
+
+	public ProgramaMap getProgramaMap() {
+		return programaMap;
 	}
+
+	public void setProgramaMap(ProgramaMap programaMap) {
+		this.programaMap = programaMap;
+	}
+
+	public List<ProgramaMap> getProgramaMaps() {
+		return programaMaps;
+	}
+
+	public void setProgramaMaps(List<ProgramaMap> programaMaps) {
+		this.programaMaps = programaMaps;
+	}
+
+	public boolean isMostrarModalMap() {
+		return mostrarModalMap;
+	}
+
+	public void setMostrarModalMap(boolean mostrarModalMap) {
+		this.mostrarModalMap = mostrarModalMap;
+	}
+
+	public List<ProgramaMap> getProgramaMapExcluidos() {
+		return programaMapExcluidos;
+	}
+
+	public void setProgramaMapExcluidos(List<ProgramaMap> programaMapExcluidos) {
+		this.programaMapExcluidos = programaMapExcluidos;
+	}	
+	
+	
 }
 
