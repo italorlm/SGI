ce95c609e0bf330e3d178218770dd396a384f853
Italo Medeiros <italo.medeiros.rlm@gmail.com>
2015-01-26 23:22:42 -0300

    Adição de metodo para impressão do Manual do Sistema.

--- sgi/src/controller/PerfilController.java
+++ sgi/src/controller/PerfilController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 import model.Perfil;
 
@@ -23,13 +30,13 @@ public class PerfilController extends GenericController<Perfil, PerfilDao> {
 	public PerfilController(){
 		injetaDao();
 		filtro = new Perfil();
+		trazerTodos = true;
 	}
-
+	
 	@Override
 	public void setaNavegacao() {
 		LISTAGEM = "listarPerfil";
 		CADASTRO = "cadastrarPerfil";
-
 	}
 
 	@Override
@@ -40,7 +47,6 @@ public class PerfilController extends GenericController<Perfil, PerfilDao> {
 			e.printStackTrace();
 		}
 	}
-
 
 	public List<SelectItem> getSelectItems() {
 		selectItems = new ArrayList<SelectItem>();
@@ -59,6 +65,53 @@ public class PerfilController extends GenericController<Perfil, PerfilDao> {
 	@Override
 	public void filtrarSuggestionBox(String userInput) {
 		// TODO Auto-generated method stub		
+	}
+	
+	public void imprimirManual() throws IOException{
+		try {
+			ServletContext ctx = (ServletContext) FacesContext
+					.getCurrentInstance().getExternalContext().getContext();
+			File f = new File(ctx.getRealPath("/publicacoes/MANUAL_SGI.pdf"));
+			HttpServletResponse response = (HttpServletResponse) FacesContext
+					.getCurrentInstance().getExternalContext().getResponse();
+			response.setContentType("application/pdf");
+			response.setHeader("content-disposition",
+					"attachment;filename=Manual_SGI.pdf");
+			byte[]bytes = bytesFromFile(f);
+			response.setContentLength(bytes.length);
+			ServletOutputStream servletStream = response.getOutputStream();
+			servletStream.write(bytes, 0, bytes.length);
+			servletStream.flush();
+			servletStream.close();
+			FacesContext.getCurrentInstance().responseComplete();
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static byte[] bytesFromFile(File file) throws IOException {
+		FileInputStream is = new FileInputStream(file);
+		long length = file.length();
+
+		if (length > Integer.MAX_VALUE) {
+			System.out.println("Sorry! Your given file is too large.");
+			System.exit(0);
+		}
+
+		byte[] bytes = new byte[(int)length];
+		int offset = 0;
+		int numRead = 0;
+		while (offset < bytes.length && (numRead=is.read(bytes, 
+				offset, bytes.length-offset)) >= 0) {
+			offset += numRead;
+		}
+		if (offset < bytes.length) {
+			throw new IOException("Could not completely read file "
+					+ file.getName());
+		}
+		is.close();
+		return bytes;
 	}
 
 }
