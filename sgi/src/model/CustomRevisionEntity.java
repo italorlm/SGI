c1b9c474dbaf007553a4fc1275c9d850f721a3a0
Italo Medeiros <italo.medeiros.rlm@gmail.com>
2015-01-26 23:37:16 -0300

    Entidade de auditoria personalizada.

---
+++ sgi/src/model/CustomRevisionEntity.java
@@ -0,0 +1,102 @@
+package model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.envers.DefaultRevisionEntity;
+import org.hibernate.envers.RevisionEntity;
+import org.hibernate.envers.RevisionNumber;
+import org.hibernate.envers.RevisionTimestamp;
+
+import util.CustomRevisionListener;
+
+@Entity
+@Table(name="revinfo")
+@RevisionEntity(CustomRevisionListener.class)
+public class CustomRevisionEntity {
+
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	Valores revtype
+		0 - Criado/Novo
+		1 - Editado
+		2 - Excluido
+	*/
+	
+	@Id
+	@GeneratedValue
+	@RevisionNumber
+	private Long id;
+	
+	@RevisionTimestamp
+	private Date data;
+	
+	private String usuario;
+	
+	private String nome;
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomRevisionEntity other = (CustomRevisionEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+	
+	
+	
+}
---
+++ sgi/src/util/CustomRevisionListener.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.util.Date;
+
+import model.CustomRevisionEntity;
+import model.Usuario;
+
+import org.hibernate.envers.RevisionListener;
+
+public class CustomRevisionListener implements RevisionListener {
+
+	@Override
+	public void newRevision(Object revisionEntity) {
+		CustomRevisionEntity revision = (CustomRevisionEntity) revisionEntity;
+		Usuario usuario = (Usuario) FacesUtils.getSession().getAttribute("usuario");
+		revision.setUsuario(usuario.getLogin());
+		revision.setNome(usuario.getNome());
+		revision.setData(new Date());
+	}
+
+}
