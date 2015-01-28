41f36cfbe9678598364d4b672906b259bb1accdb
Italo Medeiros <italo.medeiros.rlm@gmail.com>
2015-01-27 22:50:39 -0300

    Exclusão de metodos.

--- sgi/src/controller/PerfilController.java
+++ sgi/src/controller/PerfilController.java
@@ -65,53 +65,5 @@ public class PerfilController extends GenericController<Perfil, PerfilDao> {
 	@Override
 	public void filtrarSuggestionBox(String userInput) {
 		// TODO Auto-generated method stub		
-	}
-	
-	public void imprimirManual() throws IOException{
-		try {
-			ServletContext ctx = (ServletContext) FacesContext
-					.getCurrentInstance().getExternalContext().getContext();
-			File f = new File(ctx.getRealPath("/publicacoes/MANUAL_SGI.pdf"));
-			HttpServletResponse response = (HttpServletResponse) FacesContext
-					.getCurrentInstance().getExternalContext().getResponse();
-			response.setContentType("application/pdf");
-			response.setHeader("content-disposition",
-					"attachment;filename=Manual_SGI.pdf");
-			byte[]bytes = bytesFromFile(f);
-			response.setContentLength(bytes.length);
-			ServletOutputStream servletStream = response.getOutputStream();
-			servletStream.write(bytes, 0, bytes.length);
-			servletStream.flush();
-			servletStream.close();
-			FacesContext.getCurrentInstance().responseComplete();
-
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	
-	public static byte[] bytesFromFile(File file) throws IOException {
-		FileInputStream is = new FileInputStream(file);
-		long length = file.length();
-
-		if (length > Integer.MAX_VALUE) {
-			System.out.println("Sorry! Your given file is too large.");
-			System.exit(0);
-		}
-
-		byte[] bytes = new byte[(int)length];
-		int offset = 0;
-		int numRead = 0;
-		while (offset < bytes.length && (numRead=is.read(bytes, 
-				offset, bytes.length-offset)) >= 0) {
-			offset += numRead;
-		}
-		if (offset < bytes.length) {
-			throw new IOException("Could not completely read file "
-					+ file.getName());
-		}
-		is.close();
-		return bytes;
-	}
-
+	}	
 }
