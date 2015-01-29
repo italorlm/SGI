c90a0948caf1e34a4b88108a4a214b2b2c432012
Italo Medeiros <italo.medeiros.rlm@gmail.com>
2015-01-29 00:47:27 -0300

    Adição de novos atributos e mudança no hashcode e equals.

--- sgi/src/model/Funcionario.java
+++ sgi/src/model/Funcionario.java
@@ -28,11 +28,17 @@ public class Funcionario extends BaseModel{
 	
 	Integer situacao;
 	
-	@ManyToOne
-	@JoinColumn(name="unidade_cptr_id")
-	UnidadeCptr unidadeCptr;
+	String matricula;
 	
 	String telefone;
+	
+	String celular;
+	
+	String email;
+	
+	@ManyToOne
+	@JoinColumn(name="unidade_cptr_id")
+	UnidadeCptr unidadeCptr;	
 	
 	@ManyToOne
 	@JoinColumn(name="cargo_id")
@@ -40,6 +46,14 @@ public class Funcionario extends BaseModel{
 	
 	@Column(name="data_admissao")
 	Date dataAdmissao;
+	
+	public Funcionario() {}
+	
+	public Funcionario(String nome, String matricula, String email) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.email = email;
+	}
 
 	public Long getId() {
 		return id;
@@ -96,12 +110,38 @@ public class Funcionario extends BaseModel{
 	public void setCargo(Cargo cargo) {
 		this.cargo = cargo;
 	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((matricula == null) ? 0 : matricula.hashCode());
 		return result;
 	}
 
@@ -119,8 +159,11 @@ public class Funcionario extends BaseModel{
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		if (matricula == null) {
+			if (other.matricula != null)
+				return false;
+		} else if (!matricula.equals(other.matricula))
+			return false;
 		return true;
 	}
-
-	
 }
